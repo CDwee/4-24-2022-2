a82819076fe04c0ade45d5853ea419aa0c93d453
CDwee <96454637+CDwee@users.noreply.github.com>
2022-04-24 20:21:22 -0500

    Update and rename README.md to main.cpp

---
+++ main.cpp
@@ -0,0 +1,45 @@
+// Started at 7:15 4-24-2022
+
+#include <iostream>
+
+int main()
+{
+    std::cout << ("Hello world" / 125) << std::endl;
+    return 0;
+}
+
+#include <iostream>
+
+int main() {
+    
+    int favorite_number;
+    
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
+#include <iostream>
+
+extern int x;
+
+int main() {
+    
+    std::cout << "Hello world" << std::endl;
+    
+    std::cout << x;
+    
+    return 0;
+}
+
+#include <iostream>
+
+int main() {
+    int favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    return 0;
+}
+
+// Ended at 8:21 4-24-2022
